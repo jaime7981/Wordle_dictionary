70cf4328e75650e9c2af42b464e4dd398f7fd00b
jaime7981 <jmfernandez1@miuandes.cl>
2022-08-02 20:09:18 -0400

    initial commit

---
+++ __main__.py
@@ -0,0 +1,135 @@
+import game_api, random, time, re
+
+#If GAME_ID == -1 show all games
+GAME_ID = 4
+
+def main():
+    total_time = 0
+    time_init = time.time()
+    
+    selected_game = game_api.SelectGame(GAME_ID)
+
+    time_interval = time.time() - time_init
+    total_time += time_interval
+    print('api request time: ' + str(time_interval))
+
+    if selected_game != None:
+        #game data
+        time_init = time.time()
+
+        word_length = selected_game['word_length']
+        words_count = selected_game['words_count']
+        game_dictionary = game_api.LoadDictionary(selected_game['language'])
+        
+        time_interval = time.time() - time_init
+        total_time += time_interval
+        print('api get dict time: ' + str(time_interval))
+
+        #dict processing
+        time_init = time.time()
+
+        game_dictionary = basic_filter(game_dictionary, word_length)
+        game_chars = character_analyzer(game_dictionary)
+        
+        first_word = SetFirstWord(game_dictionary, word_length, game_chars)
+        print('first word guess: ' + first_word)
+        
+        time_interval = time.time() - time_init
+        total_time += time_interval
+        print('word pre procesing time: ' + str(time_interval))
+
+        #play game
+        time_init = time.time()
+        
+        response = FirstTurn(first_word)
+
+        time_interval = time.time() - time_init
+        total_time += time_interval
+        print('first turn time: ' + str(time_interval))
+
+        time_init = time.time()
+
+        response = Play(game_dictionary, word_length, words_count, response)
+
+        time_interval = time.time() - time_init
+        total_time += time_interval
+        print('second turn time: ' + str(time_interval))
+        print('total time: ' + str(total_time))
+
+def basic_filter(game_dictionary, word_length):
+    new_dict = []
+    for word in game_dictionary:
+        if len(word) == word_length:
+            new_dict.append(word)
+    print('word length filter: ' + str(len(new_dict)))
+    return new_dict
+
+def character_analyzer(game_dictionary):
+    char_analyzer = {}
+    for word in game_dictionary:
+        for letter in word:
+            if letter not in char_analyzer:
+                char_analyzer[letter] = 1
+            else:
+                char_analyzer[letter] += 1
+    char_analyzer = dict(sorted(char_analyzer.items(), key=lambda x: x[1], reverse=True))
+    print(char_analyzer)
+    return char_analyzer
+
+def SetFirstWord(game_dictionary, word_length, characters):
+    key_list = list(characters.keys())
+    first_results = []
+    aux_list = []
+    first_letter = True
+
+    for iterator in range(word_length):
+        if first_letter == True:
+            for words in game_dictionary:
+                if key_list[iterator] in words:
+                    first_results.append(words)
+            first_letter = False
+        else:
+            for words in first_results:
+                if key_list[iterator] in words:
+                    aux_list.append(words)
+            if len(aux_list) > 1:
+                first_results = aux_list
+            else:
+                break
+            aux_list = []
+    print('first best len: ' + str(len(first_results)))
+    print('first best list: ' + str(first_results))
+
+    return first_results[random.randint(0, len(first_results))]
+
+def FirstTurn(first_word):
+    response = game_api.SendWord(GAME_ID, first_word)
+
+    finished = response['finished']
+    if finished == True:
+        print('game ended')
+        game_api.ResetGame(GAME_ID)
+        exit()
+    else:
+        return response
+
+def Play(game_dictionary, word_length, words_count, response):
+    new_selected_word = ''
+    
+    result = response['result']
+    words_state = response['words_state']
+
+
+    #response = game_api.SendWord(GAME_ID, new_selected_word)
+
+    if response['finished'] == True:
+        print('game ended')
+        game_api.ResetGame(GAME_ID)
+        exit()
+    else:
+        #Play(game_dictionary, word_length, words_count, response)
+        print('Recursive turn')
+        pass
+
+if __name__ == '__main__':
+    main()
---
+++ game_api.py
@@ -0,0 +1,71 @@
+import requests, numpy, os.path
+
+BASE_URL = 'https://pds-wordie.herokuapp.com'
+PLAYER_KEY = 'GGOQBVS'
+
+#Get Active Games
+def ShowGames():
+    response = requests.get(f'{BASE_URL}/api/games/')
+    games = response.json()['games']
+    for g in games:
+        print(g)
+
+def SelectGame(game_id):
+    response = requests.get(f'{BASE_URL}/api/games/')
+    games = response.json()['games']
+    for g in games:
+        if game_id == g['id']:
+            return g
+        elif game_id == -1:
+            print(g)
+    return None
+
+def LoadDictionary(language):
+    lang_filename = 'static/' + language.split('/')[2]
+    if os.path.isfile(lang_filename):
+        print ("File exist")
+        try:
+            f = open(lang_filename)
+        except IOError:
+            print("File not accessible")
+        finally:
+            saved_file = list(f)
+            counter = 0
+            for word in saved_file:
+                saved_file[counter] = word.strip('\n')
+                counter += 1
+            f.close()
+            return saved_file
+    else:
+        print ("File does not exist")
+        file_url = f'{BASE_URL}{language}'
+        r = requests.get(file_url)
+        requests.enconding = 'cp1252'
+        text = r.content.decode('cp1252', errors='ignore')
+        text_list = text.split('\n')
+        words = [w.strip().upper() for w in text_list]
+        print('dict length: ' + str(len(words)))
+        SaveDisctionary(words, lang_filename)
+        return words
+
+def SaveDisctionary(dictionary, name):
+    numpy.savetxt(f"{name}", dictionary, delimiter=",", fmt='%s')
+
+def SendWord(game_id, word):
+    data = {
+        'game': game_id,
+        'key': PLAYER_KEY,
+        'word': word
+    }
+    r = requests.post(f'{BASE_URL}/api/play/', data=data)
+    print(r.json())
+    return r.json()
+
+def ResetGame(game_id):
+    data = {
+        'game': game_id,
+        'key': PLAYER_KEY
+    }
+    r = requests.post(f'{BASE_URL}/api/reset/', data=data)
+    print(r.json())
+    return r.json()
